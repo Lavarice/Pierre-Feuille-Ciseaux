94b8ff11b81e8cedafdd54fcd8e441cc7ad3885f
lobsteur@gmail.com <lobsteur@gmail.com>
2024-12-13 14:23:08 +0100

    Version finis

--- index.js
+++ index.js
@@ -4,44 +4,41 @@ let btnFeuille = document.getElementById('f');
 let btnCiseau = document.getElementById('c');
 let choixOrdi = document.getElementById('choixOrdi');
 let ordinateur = document.getElementById('win');
-const choixPlayer= ["Pierre","Feuille","Ciseaux"];
+const choixPlayer = ["Pierre","Feuille","Ciseaux"];
 let rollBack = document.getElementById('picture');
-
-
-
 
 function computer(){
     randomChoice = Math.floor(Math.random() * choixPlayer.length);
     return(choixPlayer[randomChoice]);
 }
 
+let valOrdi = computer();
 
-const cap = computer();
-
-function game(moi){
+function game(moi, valOrdi){
 
     let message;
 
-    switch(moi + '_' +cap){
+    switch(moi + '_' + valOrdi){
         case "Pierre_Ciseaux":
         case "Feuille_Pierre":
         case "Ciseaux_Feuille":
-            message = `Vous avez choisi ${moi}, l'ordinateur a choisi ${cap}. 🏆 Vous gagnez !`;
+            message = `J'ai choisi ${moi}, l'ordinateur a choisi ${valOrdi}. 😎​​ Je suis trop fort! 😎​`;
             break;
 
         case "Pierre_Feuille":
         case "Feuille_Ciseaux":
-        case "Ciseaux_Pierre":
-            message = `Vous avez choisi ${moi}, l'ordinateur a choisi ${cap}. 😢 Vous perdez !`;
+        case "Ciseaux_Pierre":  
+            message = `J'ai choisi ${moi}, l'ordinateur a choisi ${valOrdi}. 😭​ T'es NULLLL Mitroglou! 😭​`;
             break;
         
         case "Pierre_Pierre":
         case "Feuille_Feuille":
         case "Ciseaux_Ciseaux":
-            message = `Vous avez choisi ${moi}, l'ordinateur a choisi ${cap}. 🤝 C'est une égalité.`;
+            message = `J'ai choisi ${moi}, l'ordinateur a choisi ${valOrdi}. 🤝 C'est une égalité. 🤝`;
             break;
     }
     ordinateur.innerText  = message;
+    
 }
 
 function reset(){
@@ -50,24 +47,29 @@ function reset(){
     choixOrdi.innerText ='';
 }
 
+
 rollBack.addEventListener('click', () =>{
-    reset();
+    reset();    
 });
 
 btnPierre.addEventListener('click', () => {
-    choix.innerText = 'Pierre'
-    choixOrdi.textContent = `${cap}`;
-    game("Pierre")
+    choix.innerText = 'Pierre';
+    let valOrdi = computer();
+    choixOrdi.textContent = valOrdi;
+    game("Pierre",valOrdi)
+    
 });
 
 btnFeuille.addEventListener('click', () => {
     choix.innerText = 'Feuille'
-    choixOrdi.textContent = `${cap}`;
-    game("Feuille")
+    let valOrdi = computer();
+    choixOrdi.textContent = valOrdi;
+    game("Feuille", valOrdi)
 });
 
 btnCiseau.addEventListener('click', () => {
     choix.innerText = 'Ciseaux';
-    choixOrdi.textContent = `${cap}`;
-    game("Ciseaux");
+    let valOrdi = computer()
+    choixOrdi.textContent = valOrdi;
+    game("Ciseaux", valOrdi);
 });
